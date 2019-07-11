421a800cec0d913b5bf6cadda7e7bec0e68e4f69
njanma <njanma1@gmail.com>
2019-07-11 23:01:25 +0300

    upd repository

--- src/main/scala/njanma/repository/TableRepository.scala
+++ src/main/scala/njanma/repository/TableRepository.scala
@@ -7,21 +7,20 @@ import doobie.free.connection.ConnectionIO
 import doobie.implicits._
 import fs2.Stream
 import njanma.entity.Table
+import njanma.repository.TableRepository.SQL
 
 class TableRepository(connector: DbConnector) {
+  type TableUpd = (String, Int, Long, Long)
+
   val xa: Transactor[IO] = connector.xa
 
-  def save(table: Table): ConnectionIO[Table] = table match {
-    case Table(_, name, participants, None) =>
-      insertOrUpdate(sql"""insert into "table"(name, participants)
-              values($name, $participants)""")
-    case Table(_, name, participants, Some(ord)) =>
-      insertOrUpdate(sql"""insert into "table"(name, participants, ordering)
-              values($name, $participants, $ord)""")
-  }
-  def update(tables: List[Table]): Stream[doobie.ConnectionIO, Table] = {
-    Param
-    Update[Table](s"""update "table"
+  def save(table: Table): ConnectionIO[Table] = insertOrUpdate(SQL.insert(table))
+
+  def update(table: Table): ConnectionIO[Table] = insertOrUpdate(SQL.update(table))
+
+  def update(tables: List[Table]): Stream[doobie.ConnectionIO, Table] =
+    Update[TableUpd](
+      s"""update "table"
             set name=?, participants=?, ordering=?
             where id = ?""")
       .updateManyWithGeneratedKeys[Table](
@@ -29,19 +28,7 @@ class TableRepository(connector: DbConnector) {
         "name",
         "participants",
         "ordering"
-      )(tables)
-  }
-
-  def update(table: Table): ConnectionIO[Table] = table match {
-    case Table(Some(id), name, participants, Some(ord)) =>
-      insertOrUpdate(sql"""update "table"
-            set name=$name, participants=$participants, ordering=$ord
-            where id = $id""")
-    case Table(Some(id), name, participants, None) =>
-      insertOrUpdate(sql"""update "table"
-            set name=$name, participants=$participants
-            where id = $id""")
-  }
+      )(tables.map(table => (table.name, table.participants, table.ordering.getOrElse(0), table.id.get): TableUpd))
 
   def getAllAfterId(afterId: Long): ConnectionIO[List[Table]] =
     sql"""select * from "table" where id > $afterId"""
@@ -66,4 +53,21 @@ class TableRepository(connector: DbConnector) {
     sql
       .updateWithLogHandler(LogHandler.jdkLogHandler)
       .withUniqueGeneratedKeys[Table]("id", "name", "participants", "ordering")
+}
+
+object TableRepository {
+
+  object SQL {
+    def update(table: Table): Fragment = table match {
+      case Table(Some(id), name, participants, maybeOrd) =>
+        sql"""update "table"
+            set name=$name, participants=$participants""" ++ maybeOrd.map(ord => fr", ordering=$ord").getOrElse(fr"") ++ fr"where id = $id"
+    }
+
+    def insert(table: Table): Fragment = table match {
+      case Table(_, name, participants, maybeOrd) =>
+        sql"""insert into "table"(name, participants""" ++ maybeOrd.map(_ => fr" ,ordering)").getOrElse(fr")") ++ fr" values($name, $participants" ++ maybeOrd.map(ord => fr", $ord)").getOrElse(fr")")
+    }
+  }
+
 }
