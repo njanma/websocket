e69b7432d23f88ea7ccf41bdcc264f96cb2ce1e8
njanma <njanma1@gmail.com>
2019-07-29 23:09:58 +0300

    added flyway

--- src/main/scala/njanma/WebSocketFlow.scala
+++ src/main/scala/njanma/WebSocketFlow.scala
@@ -1,10 +1,12 @@
 package njanma
 
 import akka.NotUsed
-import akka.actor.{ActorRef, ActorSelection}
+import akka.actor.ActorRef
+import akka.http.scaladsl.model.headers.{ModeledCustomHeader, ModeledCustomHeaderCompanion}
 import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import akka.http.scaladsl.server.directives.Credentials
 import akka.pattern.ask
 import akka.stream.ActorMaterializer
 import akka.stream.scaladsl.Flow
@@ -19,6 +21,7 @@ import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.Future
 import scala.concurrent.duration._
 import scala.language.postfixOps
+import scala.util.Try
 
 trait WebSocketFlow {
 
@@ -28,7 +31,11 @@ trait WebSocketFlow {
 
   def tableActor: ActorRef
 
-  def webSocketRoute: Route = path("")(handleWebSocketMessages(flow))
+  def webSocketRoute: Route = path("")(authenticateBasic(realm = "sec", userPassAuthenticator) { usr =>
+    authorizeAsync(_ => hasPermissions(usr)) {
+      handleWebSocketMessages(flow)
+    }
+  })
 
   private def flow: Flow[Message, Message, NotUsed] = {
     Flow[Message]
@@ -52,4 +59,38 @@ trait WebSocketFlow {
         out => Future(TextMessage(out.asJson.spaces2))
       )
   }
+
+  def userPassAuthenticator(credentials: Credentials): Option[User] = {
+    credentials match {
+      case p@Credentials.Provided(id) =>
+        if (p.verify("p4ssw0rd")) Some(User(id))
+        else None
+      case _ => None
+    }
+  }
+
+  case class User(name: String)
+
+  val validUsers = Set("john", "peter", "tiger", "susan")
+
+  def hasPermissions(user: User): Future[Boolean] = {
+    Future.successful(validUsers.contains(user.name))
+  }
+
+  final class ApiTokenHeader(token: String) extends ModeledCustomHeader[ApiTokenHeader] {
+    override def renderInRequests = true
+
+    override def renderInResponses = false
+
+    override val companion = ApiTokenHeader
+
+    override def value: String = token
+  }
+
+  object ApiTokenHeader extends ModeledCustomHeaderCompanion[ApiTokenHeader] {
+    override val name = "apiKey"
+
+    override def parse(value: String) = Try(new ApiTokenHeader(value))
+  }
+
 }
