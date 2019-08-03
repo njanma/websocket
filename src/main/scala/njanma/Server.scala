348ecab8b4f8e2cd330c514a53e22769d5a9683f
njanma <njanma1@gmail.com>
2019-08-03 20:05:54 +0300

    fix tests

--- src/main/scala/njanma/Server.scala
+++ src/main/scala/njanma/Server.scala
@@ -5,7 +5,7 @@ import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Route
 import akka.stream.{ActorMaterializer, ActorMaterializerSettings, Supervision}
 import njanma.actor.TableActor
-import njanma.config.{AppConfig, DbConfig, ServerConfig}
+import njanma.config.AppConfig
 import njanma.repository.{DbConnector, TableRepository, UserRepository}
 import njanma.security.UserAuthenticator
 import org.flywaydb.core.Flyway
@@ -20,8 +20,10 @@ import scala.util.{Failure, Success}
 object Server extends App with WebSocketFlow {
 
   implicit val system: ActorSystem = ActorSystem("server-system")
-  implicit val materializer: ActorMaterializer = ActorMaterializer(ActorMaterializerSettings(system)
-    .withSupervisionStrategy(_ => Supervision.Restart))
+  implicit val materializer: ActorMaterializer = ActorMaterializer(
+    ActorMaterializerSettings(system)
+      .withSupervisionStrategy(_ => Supervision.Restart)
+  )
   implicit val executionContext: ExecutionContext = system.dispatcher
 
   private val appConfig = loadConfigOrThrow[AppConfig]
@@ -31,12 +33,23 @@ object Server extends App with WebSocketFlow {
   val userRepository: UserRepository = new UserRepository(dbConnector)
   override val connectionPath: String = appConfig.server.connectionPath
 
-  override val tableActor: ActorRef = system.actorOf(TableActor.props(tableRepository), "table-actor")
-  override val userAuthenticator: UserAuthenticator = new UserAuthenticator(userRepository)
+  override val tableActor: ActorRef =
+    system.actorOf(TableActor.props(tableRepository), "table-actor")
+  override val userAuthenticator: UserAuthenticator = new UserAuthenticator(
+    userRepository
+  )
 
   lazy val routes: Route = webSocketRoute
 
-  Flyway.configure().dataSource(appConfig.db.dataSource, appConfig.db.user, appConfig.db.password).load().migrate()
+  Flyway
+    .configure()
+    .dataSource(
+      appConfig.db.dataSource,
+      appConfig.db.user,
+      appConfig.db.password
+    )
+    .load()
+    .migrate()
 
   val binding: Future[Http.ServerBinding] =
     Http().bindAndHandle(routes, appConfig.server.host, appConfig.server.port)
--- src/test/scala/njanma/WebSocketSecuritySpec.scala
+++ src/test/scala/njanma/WebSocketSecuritySpec.scala
@@ -3,59 +3,54 @@ package njanma
 import akka.actor.ActorRef
 import akka.http.scaladsl.model.StatusCodes
 import akka.http.scaladsl.model.headers.BasicHttpCredentials
-import akka.http.scaladsl.model.ws.{Message, TextMessage}
 import akka.http.scaladsl.server.AuthenticationFailedRejection
 import akka.http.scaladsl.server.directives.Credentials
 import akka.http.scaladsl.testkit.{ScalatestRouteTest, WSProbe}
 import akka.stream.ActorMaterializer
-import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.testkit.TestActors
-import njanma.dto.Request.Ping
 import njanma.entity.User
-import njanma.repository.{DbConnector, UserRepository}
+import njanma.repository.UserRepository
 import njanma.security.UserAuthenticator
 import org.scalamock.scalatest.MockFactory
-import org.scalatest.{BeforeAndAfter, BeforeAndAfterAll, Matchers, WordSpec}
+import org.scalatest._
 
-class WebSocketSecuritySpec extends WordSpec
-          with Matchers
-          with ScalatestRouteTest
-          with MockFactory
-          with BeforeAndAfterAll {
+class WebSocketSecuritySpec
+    extends WordSpec
+    with Matchers
+    with ScalatestRouteTest
+    with MockFactory
+    with OneInstancePerTest {
   private val testMaterializer = this.materializer
-  private val fakeAuthenticator = stub[FakeAuth]
+  private val fakeAuth = stub[FakeAuth]
   private val route = new WebSocketFlow {
     override implicit def materializer: ActorMaterializer = testMaterializer
-    override def tableActor: ActorRef = system.actorOf(TestActors.echoActorProps)
-    override def userAuthenticator: UserAuthenticator = fakeAuthenticator
+    override def tableActor: ActorRef =
+      system.actorOf(TestActors.echoActorProps)
+    override def userAuthenticator: UserAuthenticator = fakeAuth
     override val connectionPath: String = "connect"
   }.webSocketRoute
+  class FakeAuth extends UserAuthenticator(mock[UserRepository])
+  val fakeUser = User("john", "p4ssw0rd", "admin")
 
-
-  class FakeRepository extends UserRepository(mock[DbConnector])
-  class FakeAuth extends UserAuthenticator(mock[FakeRepository])
-
-  override protected def beforeAll(): Unit = {
-    val fakeUser = User("john", "p4ssw0rd", "admin")
-    (fakeAuthenticator.check _)
-      .when(Credentials(Some(BasicHttpCredentials("john", "p4ssw0rd"))))
-      .returns(Option(fakeUser))
-    (fakeAuthenticator.hasPermissions _)
-      .when(*)
-      .returns(true)
-  }
+  (fakeAuth.check _)
+    .when(Credentials(Some(BasicHttpCredentials("john", "p4ssw0rd"))))
+    .returns(Some(fakeUser))
+    .anyNumberOfTimes()
+  (fakeAuth.hasPermissions _).when(*).returns(true).anyNumberOfTimes()
+  (fakeAuth.check _).when(Credentials.Missing).returns(None).anyNumberOfTimes()
 
   "WebSocketFlow" must {
     "reject users without auth" in {
       val wsClient = WSProbe()
       WS("/connect", wsClient.flow) ~> route ~>
-        check(
+        check {
           rejection shouldBe a[AuthenticationFailedRejection]
-        )
+        }
     }
     "accept users with Basic Auth" in {
       val wsClient = WSProbe()
-      WS("/connect", wsClient.flow).addCredentials(BasicHttpCredentials("john", "p4ssw0rd")) ~> route ~>
+      WS("/connect", wsClient.flow)
+        .addCredentials(BasicHttpCredentials("john", "p4ssw0rd")) ~> route ~>
         check {
           isWebSocketUpgrade should be(true)
           status should be(StatusCodes.SwitchingProtocols)
